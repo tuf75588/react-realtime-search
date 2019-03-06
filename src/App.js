060d5a7aa0511a33965642ef3a324f31148a8e9c
Andrew Davis <atd285@gmail.com>
2019-03-06 18:55:08 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import styled from '@emotion/styled';
 import './App.css';
+const StyledHeader = styled.header`
+  padding: 4em;
+  background-color: #00bfb3;
+  border: 1px solid white;
+  width: 400px;
+  height: 100px;
+  text-align: center;
+`;
 
 class App extends Component {
+  state = {
+    data: [],
+    searchTerm: ''
+  };
+  componentDidMount() {
+    const endpoint =
+      'https://gist.githubusercontent.com/anonymous/1295788c7bff052a1e8a/raw/6e109604c7a7f3efe77c8048bb2fe2f3e1cdcb7b/gistfile1.json';
+    return fetch(endpoint)
+      .then((data) => {
+        return data.json();
+      })
+      .then((results) => {
+        this.setState(() => ({
+          data: results.Reggae
+        }));
+      });
+  }
+  handleChange = (e) => {
+    const val = e.target.value;
+    this.setState((prevState) => ({
+      searchTerm: val
+    }));
+  };
   render() {
+    const { searchTerm } = this.state;
+    const val = new RegExp(searchTerm, 'gi');
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <div className='container'>
+          <StyledHeader>
+            <h1>Real Time React Search</h1>
+            <h3>Here is a list of Reggae artists rendered from a JSON object</h3>
+          </StyledHeader>
+        </div>
+        <h3 className='label'>Search:</h3>
+        <input type='text' name='search' placeholder='search here' onChange={this.handleChange} />
+        <ul>
+          {!this.state.data ? (
+            <p>Loading...</p>
+          ) : (
+            this.state.data
+              .filter((item, index) => {
+                const regex = new RegExp(this.state.searchTerm, 'gi');
+                return item.match(regex);
+              })
+              .map((name, index) => {
+                return <li key={index}>{name}</li>;
+              })
+          )}
+        </ul>
       </div>
     );
   }
